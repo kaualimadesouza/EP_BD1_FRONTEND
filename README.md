86d31d596ae0d6ffc97e4d87f7f2b52438c919c4
Kaua <kaualimadesouza15@gmail.com>
2025-10-05 18:41:58 -0300

    feat: create project

---
+++ README.md
@@ -0,0 +1,1 @@
+# EP_BD1_FRONTEND
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <body>
+        <h1 className="text-4xl font-bold align-middle">Goalytics</h1>
+      </body>
+    </>
+  )
+}
+
+export default App
